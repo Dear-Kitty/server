7c2ac0c0232ad95eea54926a48ad290f5251ff7a
JYeon-p <j_yeon_p@naver.com>
2024-12-15 00:30:32 +0900

    Fix: app.ts 포트 수정

--- app.ts
+++ app.ts
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use('/users', usersRouter);
 app.use('/voca', vocaRouter);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
